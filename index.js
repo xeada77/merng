7ae1386cb871729e971f80b6163cd4f4ac8886a9
Luis Martinez <luismartinez@MacBook-Pro-de-Luis.local>
2021-03-08 17:06:49 +0100

    Primer Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config");
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => "Hello World",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDb Connect");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => console.log(`Server listen at ${res.url}`));
