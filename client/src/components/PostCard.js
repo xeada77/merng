e7d76119eb1c2137946148e352459bc4577c483b
Luis Martinez <luismartinez@MacBook-Pro-de-Luis.local>
2021-03-11 14:38:44 +0100

    Decimo Commit

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostCard = ({ post }) => {
+  return <div>{post.body}</div>;
+};
+
+export default PostCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import PostCard from "../components/PostCard";
+import { Grid } from "semantic-ui-react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    loading,
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POST_QUERY);
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts...</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
+  );
 };
+
+const FETCH_POST_QUERY = gql`
+  {
+    getPosts {
+      id
+      username
+      body
+      createdAt
+      commentCount
+      likeCount
+      comments {
+        id
+        username
+        body
+        createdAt
+      }
+      likes {
+        id
+        username
+        createdAt
+      }
+    }
+  }
+`;
 
 export default Home;
